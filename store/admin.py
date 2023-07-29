a476d69e418575082b7aac3a370d4d9c3f3fffcc
Bohdan-Pysarenko <bogdan161001@gmail.com>
2023-07-29 12:45:47 +0300

    New stuffs

--- store/admin.py
+++ store/admin.py
@@ -2,6 +2,9 @@ from .models import *
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Attribute)
@@ -9,11 +12,12 @@ admin.site.register(AttributeValue)
 admin.site.register(Variant)
 admin.site.register(ProductImage)
 admin.site.register(Review)
+admin.site.register(ReviewImage)
 
 
 
 
-@admin.register(ProductAttribute)
-class ProductAttributeAdmin(admin.ModelAdmin):
-	filter_horizontal = ['attribute', 'value']
+# @admin.register(ProductAttribute)
+# class ProductAttributeAdmin(admin.ModelAdmin):
+# 	filter_horizontal = ['attribute', 'value']
 
--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import *
 
 # Create your views here.
@@ -18,13 +19,108 @@ def home(request):
 	return render(request, 'store/home.html', context)
 
 def product_render(request, slug):
+	if request.method == 'POST':
+		product = Product.objects.get(slug=slug)
+
+		try:
+			customer = request.user.customer
+		except:
+			device = request.COOKIES['device']
+			customer, created = Customer.objects.get_or_create(device=device)
+
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+		orderItem.quantity=request.POST['quantity']
+		orderItem.save()
+
+		return redirect('/cart/')
+
+
 	product = Product.objects.get(slug=slug)
 	images = ProductImage.objects.all().filter(product=product)
 	attributes = ProductAttribute.objects.all().filter(product=product)
 	variants = Variant.objects.all().filter(product=product)
+	reviews = Review.objects.filter(product=product)
+
 	#print(attributes.value.all())
-	context = {'product': product, 'images': images, 'variants': variants}
+	context = {'product': product, 'images': images, 'variants': variants,
+		'reviews': reviews}
 	return render(request, 'store/product.html', context)
+
+def cart(request):
+	try:
+		customer = request.user.customer
+	except:
+		device = request.COOKIES['device']
+		customer, created = Customer.objects.get_or_create(device=device)
+
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	context = {'order':order}
+	return render(request, 'store/cart.html', context)
+
+def checkout(request):
+	try:
+		customer = request.user.customer
+	except:
+		device = request.COOKIES['device']
+		customer, created = Customer.objects.get_or_create(device=device)
+
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	context = {'order':order}
+	return render(request, 'store/checkout.html', context)
+
+@login_required
+def add_review(request):
+	if request.method == 'POST':
+		data = request.POST
+		images = request.FILES.getlist('images')
+
+		product = Product.objects.get(slug=data['selected_product'])
+
+		review = Review.objects.create(
+       		product=product,
+			name=data['name'],
+			text=data['text'],
+			rating=data['rating'],
+        )
+
+		for image in images:
+			photo = ReviewImage.objects.create(review=review, image=image)
+
+	products = Product.objects.all()
+	context = {'products': products}
+
+	return render(request, 'store/add_review.html', context)
+
+@login_required
+def add_product(request):
+	if request.method == 'POST':
+		data = request.POST
+		images = request.FILES.getlist('images')
+
+		slug = name_to_slug(data['name'])
+		selected_category = data['selected_category']
+		selected_category_id = Category.objects.get(slug=selected_category).id
+
+		product = Product.objects.create(
+			name=data['name'],
+			slug=slug,
+			crossed_out_price=data['crossed_out_price'],
+			price=data['price'],
+			available=data['available'],
+			description=data['description']
+		)
+
+		for image in images:
+			photo = ProductImage.objects.create(product=product, image=image)
+
+		product.category.set([selected_category_id])
+
+	categories = Category.objects.all()
+	context = {'categories': categories}
+	return render(request, 'store/add_product.html', context)
 
 def about_us(request):
 	return render(request, 'store/about_us.html')
@@ -40,3 +136,8 @@ def privacy_policy(request):
 
 def terms_of_service(request):
 	return render(request, 'store/terms_of_service.html')
+
+
+def name_to_slug(name):
+	slug = name.lower().replace(' ', '-')
+	return slug
